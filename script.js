afc333dc280f7f6307a6dda4d0ad693cfca06c84
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-24 21:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+// script.js
+const list = document.getElementById('infi-list');
+const itemsPerPage = 10;
+let currentPage = 1;
+
+function addMoreItems() {
+    // Add 2 more list items
+    for (let i = 0; i < 2; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `List Item ${currentPage * itemsPerPage + i + 1}`;
+        list.appendChild(listItem);
+    }
+    currentPage++;
+}
+
+// Add initial list items
+addMoreItems();
+
+// Event listener for scroll
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        addMoreItems();
+    }
+});
 
